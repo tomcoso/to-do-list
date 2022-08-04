5f14c8981958e33e8bd0e0581a222124127aec86
tomcoso <tomasdessy0@gmail.com>
2022-08-04 17:19:07 +0100

    start DOM module and skeleton html

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import './style.css'
 import app from './application.js'
+import page from './htmlgen.js'
 
-const test = app.Taskgroup('mytitle', 'Important')
+const test = app.Taskgroup('My First Project', 'Important')
 
 test.createTask('myTask', 'Important', 'My Fabulous Project')
 test.createTask('checkTask', 'Unimportant')
-// console.log(test.getTasks())
 const checkTest = {
   item1: false,
   item2: false,
@@ -14,5 +14,14 @@ const checkTest = {
 test.find('checkTask').setCheckbox(checkTest)
 test.find('checkTask').updateCheckbox('item1', true)
 test.find('checkTask').updateCheckbox('item2', true)
-console.log(test.find('checkTask'))
-console.log(test.find('checkTask').info.read('completed'))
+
+const test2 = app.Taskgroup(
+  'My Second Project',
+  'Unimportant',
+  'Fancy description'
+)
+test2.createTask('Task 1', 'Important')
+
+const deck = [test, test2]
+
+page.attach(deck)
