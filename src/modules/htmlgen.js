5928661d810e3dd9e921a50ff4ea43e1c5a699e6
tomcoso <tomasdessy0@gmail.com>
2022-08-07 14:53:59 +0100

    code renderTaskgroup

--- src/modules/htmlgen.js
+++ src/modules/htmlgen.js
@@ -47,13 +47,24 @@ const page = (function () {
     bodyPriority.classList.add('body-priority')
 
     const bodyDDate = document.createElement('p')
-    if (obj.dueDate) {
-      bodyDDate.textContent = 'Due: ' + obj.dueDate
-    } else {
-      bodyDDate.textContent = 'Due: Whenever'
-    }
 
-    bodyHead.append(titleWrap, bodyPriority, bodyDDate)
+    bodyDDate.textContent = 'Due: ' + (obj.dueDate || 'Whenever')
+
+    const bodyDelBtn = document.createElement('button')
+    bodyDelBtn.setAttribute('type', 'button')
+    bodyDelBtn.textContent = 'Delete'
+
+    const bodyCompleteBtn = document.createElement('button')
+    bodyCompleteBtn.setAttribute('type', 'button')
+    bodyCompleteBtn.textContent = 'Complete'
+
+    bodyHead.append(
+      titleWrap,
+      bodyPriority,
+      bodyDDate,
+      bodyDelBtn,
+      bodyCompleteBtn
+    )
 
     const bodyDesc = document.createElement('div')
     bodyDesc.classList.add('body-description')
@@ -85,6 +96,90 @@ const page = (function () {
     }
   }
 
+  const _renderTaskgroup = function (obj, layoutBody) {
+    const bodyHead = document.createElement('div')
+    bodyHead.classList.add('body-head')
+
+    const bodyTitle = document.createElement('div')
+    bodyTitle.classList.add('body-title')
+    bodyTitle.textContent = 'Taskgroup'
+
+    const bodyCDate = document.createElement('p')
+    bodyCDate.textContent = 'Created the ' + obj.creationDate
+    bodyTitle.classList.add('body-cdate')
+
+    const titleWrap = document.createElement('div')
+    titleWrap.classList.add('body-title-wrap')
+    titleWrap.append(bodyTitle, bodyCDate)
+
+    const bodyPriority = document.createElement('p')
+    bodyPriority.textContent = obj.priority
+    bodyPriority.classList.add('body-priority')
+
+    const bodyDDate = document.createElement('p')
+    bodyDDate.textContent = 'Due: ' + (obj.dueDate || 'Whenever')
+
+    const bodyDesc = document.createElement('div')
+    bodyDesc.classList.add('body-description')
+    bodyDesc.textContent = obj.description
+
+    bodyHead.append(titleWrap, bodyPriority, bodyDDate, bodyDesc)
+
+    const bodyTasks = document.createElement('div')
+    bodyTasks.classList.add('body-tasks')
+
+    const tasksHeader = document.createElement('div')
+    tasksHeader.classList.add('tasks-header')
+
+    const tasksHeaderTitle = document.createElement('p')
+    tasksHeaderTitle.textContent = `${obj.title}'s Tasks`
+
+    const tasksNewBtn = document.createElement('button')
+    tasksNewBtn.setAttribute('type', 'button')
+    tasksNewBtn.textContent = 'New Task'
+
+    tasksHeader.append(tasksHeaderTitle, tasksNewBtn)
+
+    const tasksList = document.createElement('div')
+
+    for (const task in obj.tasks) {
+      const newTask = document.createElement('div')
+      newTask.classList.add('task')
+
+      const taskTitle = document.createElement('div')
+      taskTitle.textContent = obj.tasks[task].title
+      taskTitle.classList.add('task-title')
+
+      const taskDDate = document.createElement('p')
+      taskDDate.classList.add('task-ddate')
+      taskDDate.textContent = 'Due: ' + (obj.tasks[task].dueDate || 'Whenever')
+
+      const taskPriority = document.createElement('p')
+      taskPriority.classList.add('task-priority')
+      taskPriority.textContent = obj.tasks[task].priority
+
+      const tasksDelBtn = document.createElement('button')
+      tasksDelBtn.setAttribute('type', 'button')
+      tasksDelBtn.textContent = 'Delete'
+
+      const tasksCompleteBtn = document.createElement('button')
+      tasksCompleteBtn.setAttribute('type', 'button')
+      tasksCompleteBtn.textContent = 'Complete'
+
+      newTask.append(
+        taskTitle,
+        taskDDate,
+        taskPriority,
+        tasksDelBtn,
+        tasksCompleteBtn
+      )
+
+      tasksList.append(newTask)
+    }
+    bodyTasks.append(tasksHeader, tasksList)
+    layoutBody.append(bodyHead, bodyTasks)
+  }
+
   const _render = function (obj, taskgroupName) {
     main.replaceChildren()
 
@@ -98,10 +193,11 @@ const page = (function () {
 
     const layoutBody = document.createElement('div')
     layoutBody.classList.add('main-body')
+    // code if block for incoming view (maybe use taskgroupName)
+    taskgroupName
+      ? _renderTask(obj, layoutBody)
+      : _renderTaskgroup(obj, layoutBody)
 
-    if (taskgroupName) {
-      _renderTask(obj, layoutBody)
-    }
     main.append(layoutHead, layoutBody)
   }
 
